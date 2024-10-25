7fba79dbc7960db56cf23379b6f3be9ac858bfc7
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-10-26 01:54:19 +0530

    frontend init

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}", // Ensure all file types are covered
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
