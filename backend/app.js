c5d292563517d031dc0da5da57f6860d0a4f3d57
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-10-26 01:55:21 +0530

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const todoRoutes = require('./routes/todoRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/todos', todoRoutes);
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+console.log('Mongo URI:', MONGO_URI); // Log the Mongo URI
+
+mongoose.connect(MONGO_URI, { })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
